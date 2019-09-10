9814dd76057f5e54d6c9dd890d4926a4fd9e28d3
Pritika <Pritikaroy@gmail.com>
2019-09-10 13:31:59 -0400

    Second page - giflist edited

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import GifResult from "./GifResult";
-import Navigation from "./Navigation";
-import Options from "./Options";
 import Form from "./Form";
 
 const GifList = (props, { match }) => {
@@ -11,10 +9,7 @@ const GifList = (props, { match }) => {
     return (
         <div>
             <h2>Search for your Gif:</h2>
-            <Form />
-            <Navigation />
-            <Options />
-            
+            <Form />            
             <GifResult term={term} />
         </div>
     );
